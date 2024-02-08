d6b6e9209e83d68ed0fa48551598d2b5c93a7647
Zeynep <zeynepakbalikk@gmail.com>
2024-02-08 23:42:09 +0300

    feat: create realtors model

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class RealtorsModel(models.Model):
+    name= models.CharField(
+        max_length=200,
+        verbose_name="Name"
+    )
+    photo = models.ImageField(
+        upload_to='media/',
+        verbose_name="Photo"
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name="Description"
+    )
+    email = models.EmailField(
+        verbose_name="Email"
+    )
+    phone = models.CharField(
+        max_length=20,
+        verbose_name="Phone"
+    )
+    is_mvp = models.BooleanField(
+        default=False,
+        verbose_name="Is MVP"
+    )
+    hire_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Hire Date"
+    )
+
+    def __str__(self):
+        return self.name
+
