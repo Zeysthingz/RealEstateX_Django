b056271e7c2120084061c4f28c8ead706e8c326a
Zeynep <zeynepakbalikk@gmail.com>
2024-02-09 00:19:29 +0300

    feat: add realtors model to admin panel

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import RealtorsModel
 
-# Register your models here.
+
+@admin.register(RealtorsModel)
+class RealtorsModelAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'email',
+        'description',
+        'phone',
+        'is_mvp',
+        'created_at',
+        'updated_at',
+        'photo',
+    ]
+    search_fields = [
+        'name',
+        'email',
+        'phone',
+        'created_at',
+    ]
+    list_editable = [
+        'name',
+        'email',
+        'description',
+        'phone',
+        'is_mvp',
+    ]
+
+    class Meta:
+        model = RealtorsModel
--- realtors/models.py
+++ realtors/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
 
-class RealtorsModel(models.Model):
-    name= models.CharField(
+class AbstractModel(models.Model):
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        blank=True,
+        verbose_name='Updated',
+        help_text='',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=True,
+        verbose_name='Created',
+        help_text='',
+    )
+
+    class Meta:
+        abstract = True
+
+
+class RealtorsModel(AbstractModel):
+    name = models.CharField(
         max_length=200,
         verbose_name="Name"
     )
@@ -25,11 +43,5 @@ class RealtorsModel(models.Model):
         default=False,
         verbose_name="Is MVP"
     )
-    hire_date = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name="Hire Date"
-    )
-
     def __str__(self):
         return self.name
-
